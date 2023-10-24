f6d482834547d6ca12dc91c6132bd52e50984c90
Alex Thomas <Alex-thomas92@outlook.com>
2023-10-24 23:40:16 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -1,20 +1,44 @@
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
 import express from "express";
+import imagemin from "imagemin";
+import imageminJpegtran from "imagemin-jpegtran";
+import imageminPngquant from "imagemin-pngquant";
+import fetch from "node-fetch";
+import puppeteer from "puppeteer";
+import xml2js from "xml2js";
+dotenv.config();
+
 const app = express();
 
-// check out the example code in the `./routes/example.js` file.
-// You can use this code to separate your routes into different files
-// which makes it easier to manage your code as it grows.
+const SUPABASE_URL = process.env.SUPABASE_URL; // Replace with your Supabase URL
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY; // Replace with your Supabase anon key
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+async function autoScroll(page) {
+  return page.evaluate(async () => {
+    return new Promise((resolve, reject) => {
+      let totalHeight = 0;
+      const distance = 100;
+      const timer = setInterval(() => {
+        const scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+        if (totalHeight >= scrollHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 1000);
+    });
+  });
+}
+
 import { ExampleRouter } from "./routes/router_example.js";
 app.use("/", ExampleRouter);
 
-// Middleware is a function that runs on every request that is sent to your app.
-// It can be used to modify the request or response objects, or to run some code
-// generally middleware is used for things like authentication,
-// logging, or parsing the request body
-//
 // the example shown below will be for logging, and it is run on every request
 app.use((req, res, next) => {
-  // try it out at http://localhost:3000/?query=hello+world and check the console
   const start = Date.now();
   res.on("finish", () => {
     const responseTime = Date.now() - start;
@@ -32,20 +56,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// you can declare a route using the syntax below:
-// app.get("path", callback)
-//
-// the callback function takes two parameters:
-// req: the request object
-// res: the response object
-//
-// req has information about the request that was made, like the url, the headers, the body, etc.
-// res has methods that allow you to send a response back to the client, like res.send() or res.json()
-//
-// below is an example of a text response and a json response using the res.send() and res.json() methods
-//
-// other methods are available, like res.render() to render a template, or res.redirect() to redirect to another url
-// see the express docs for more info: https://expressjs.com/en/api.html#res
 app.get("/", (req, res) => {
   res.send("Choo Choo! Welcome to your Express app 🚅");
 });
@@ -54,24 +64,131 @@ app.get("/json", (req, res) => {
   res.json({ "Choo Choo": "Welcome to your Express app 🚅" });
 });
 
-// You can declare a POST route using the syntax below:
-// POST requests are different from GET requests because they have a body
-// and are generally used to create or send data to the server
-// Check out the MDN docs for more: https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
 app.post("/post", (req, res) => {
   res.json({ "Choo Choo": "Welcome to your Express app 🚅", body: req.body });
 });
 
-// the port that the process listens on is automatically set by railway,
-// so you don't need to set the PORT manually on your service.
-// it defaults to port 3000 when the PORT environment variable is not set,
-// so you can access your app at http://localhost:3000 when running locally with `npm run dev`
-//
-// if you want to have the same environment variables that you've set on the railway dashboard,
-// check out the cli at https://github.com/railwayapp/cli
-const port = process.env.PORT || 3000;
+app.get("/create-bucket", async (req, res) => {
+  const bucketName = req.query.bucketName;
+  const { data, error } = await supabase.storage.createBucket(bucketName, {
+    public: false,
+    allowedMimeTypes: ["image/png"],
+    fileSizeLimit: 1024,
+  });
+  console.log({ data, error });
+  res.json({ data, error });
+});
 
-// the final step is to start your app using the following code:
+app.get("/capture", async (req, res) => {
+  const url = req.query.url;
+  const resolutions = req.query.resolutions.split(",");
+
+  // Fetch and parse the sitemap
+  const sitemapResponse = await fetch(`${url}/sitemap.xml`);
+  const sitemapXml = await sitemapResponse.text();
+  const sitemapJson = await xml2js.parseStringPromise(sitemapXml);
+
+  let urls = [];
+  if (sitemapJson.urlset) {
+    // Regular sitemap
+    urls = sitemapJson.urlset.url.map((u) => u.loc[0]);
+  } else if (sitemapJson.sitemapindex) {
+    // Sitemap index
+    const sitemapUrls = sitemapJson.sitemapindex.sitemap.map((s) => s.loc[0]);
+    for (let sitemapUrl of sitemapUrls) {
+      const sitemapResponse = await fetch(sitemapUrl);
+      const sitemapXml = await sitemapResponse.text();
+      const sitemapJson = await xml2js.parseStringPromise(sitemapXml);
+      if (sitemapJson.urlset) {
+        urls.push(...sitemapJson.urlset.url.map((u) => u.loc[0]));
+      }
+    }
+  }
+
+  urls = [...new Set(urls)];
+
+  console.log({ urls, resolutions });
+
+  // Launch Puppeteer
+  const browser = await puppeteer.launch();
+
+  const allPromises = [];
+
+  for (let pageUrl of urls) {
+    for (let resolution of resolutions) {
+      allPromises.push(
+        (async () => {
+          const page = await browser.newPage();
+          try {
+            const [width, height] = resolution.split("x").map(Number);
+            await page.setViewport({ width, height });
+            await page.goto(pageUrl, {
+              waitUntil: "networkidle0",
+            });
+
+            await autoScroll(page);
+
+            const screenshot = await page.screenshot({ fullPage: true });
+
+            // Adjusted websiteFolder and pageName extraction
+            const urlParts = pageUrl
+              .replace(/https?:\/\/(www\.)?/, "")
+              .split("/");
+            const websiteFolder = urlParts[0]; // Assumes the domain is always the first part
+            const pageName = urlParts.slice(1).join("_") || "index"; // Joins the rest of the path, or defaults to "index"
+
+            const resolutionFolder = resolution;
+            const screenshotName = `${pageName}.png`;
+            /*  */
+            const storagePath = `${websiteFolder}/${resolutionFolder}/${screenshotName}`;
+            console.log({ storagePath });
+            const { error: uploadError } = await supabase.storage
+              .from("screenshots")
+              .upload(storagePath, screenshot);
+
+            if (uploadError) {
+              console.error(
+                "Failed to upload screenshot to Supabase:",
+                uploadError.message
+              );
+            } else {
+              console.log("Screenshot uploaded to Supabase:", storagePath);
+            }
+            /*  */
+
+            // await imagemin([filePath], {
+            //   destination: dirPath,
+            //   plugins: [
+            //     imageminJpegtran(),
+            //     imageminPngquant({
+            //       quality: [0.6, 0.8],
+            //     }),
+            //   ],
+            // });
+
+            // console.log("Screenshot saved:", filePath);
+          } catch (error) {
+            console.error(
+              `Failed to process ${pageUrl} at resolution ${resolution}:`,
+              error.message
+            );
+          } finally {
+            await page.close();
+          }
+        })()
+      );
+    }
+  }
+
+  // Wait for all promises to resolve
+  await Promise.all(allPromises);
+
+  await browser.close();
+  res.send("Screenshots captured and saved locally.");
+});
+
+const port = process.env.PORT || 3001;
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
